122946562fdcd0aacf5dc04c3cc64bbd94ca109b
mishrapintu8800 <mishrapintu8800@gmail.com>
2022-07-15 02:45:37 +0530

    a

--- app/controller/homecontroller.js
+++ app/controller/homecontroller.js
@@ -136,12 +136,6 @@ if(req.body.booking_date !== undefined){
 exports.loyalty_program = function(req, res) {
 session=req.session;
 console.log(req.body);
-if(req.body.booking_date !== undefined){
-    config.con.query("INSERT INTO `meeet_and_event`(`booking_date`, `booking_time`, `no_of_guest`, `name`, `email`, `phone`, `comment`) VALUES ('"+req.body.booking_date+"','"+req.body.booking_time+"','"+req.body.no_of_guest+"','"+req.body.name+"','"+req.body.email+"','"+req.body.phone+"','"+req.body.comment+"')",(err,result) => {
-        if(err) console.log(err);
-        res.redirect('meet-and-events');
-    });
-}else{
     if(session.user_id !== undefined){
         config.con.query("SELECT * FROM user WHERE id="+session.user_id,(err,result) => {
             if(err){res.redirect('/logout');}else{
@@ -157,7 +151,6 @@ if(req.body.booking_date !== undefined){
         let user = '';
         res.render('loyalty_program',{APP_URL : config.APP_URL,url:req.url,user:user});
     }
-}
 }
 exports.e_gift_coupon = function(req, res) {
 session=req.session;
@@ -251,7 +244,13 @@ var coupon = one+tow+three+four+five+Date.now();
 const date = require('date-and-time');
 const now = new Date();
 var date_of_purchase = date.format(now,'YYYY/MM/DD HH:mm:ss');
-var photo_id_card = '';
+var photo_id_card;
+photo_id_card = [];
+    req.files.forEach(uploads => {
+        if(uploads.fieldname == 'photo_id_card'){
+            photo_id_card.push(uploads.filename);
+        }
+    });
 config.con.query("INSERT INTO `loyalty_program`(`name`, `mobile`, `email`, `date_of_purchase`, `occupation`, `no_of_person_in_family`, `photo_id_card`, `hotel_id`, `full_communication_address`, `coupon_no`, `member_type`) VALUES ('"+req.body.name+"','"+req.body.mobile+"','"+req.body.email+"','"+date_of_purchase+"','"+req.body.occupation+"','"+req.body.no_of_person_in_family+"','"+photo_id_card+"','"+req.body.hotel_id+"','"+req.body.full_communication_address+"','"+coupon+"','"+req.params.id+"')",(err,result) => {
         config.con.query("SELECT * FROM loyalty_program WHERE id="+result.insertId,(err,resulta) => {
             if(err){res.redirect('/loyalty-program');}else{
@@ -329,7 +328,21 @@ if(session.user_id !== undefined){
 }
 exports.career = function(req, res) {
 session=req.session;
-            let user = '';
+let user = '';
+if(req.body.name !== undefined){
+    var cv;
+        cv = [];
+    req.files.forEach(uploads => {
+        if(uploads.fieldname == 'cv'){
+            cv.push(uploads.filename);
+        }
+    });
+    // console.log(req.files);
+    config.con.query("INSERT INTO `career`(`name`, `mobile`, `email`, `current_position`, `language`, `skill`, `current_salary`, `apply_for`, `expected_salary`, `cv`, `message`) VALUES ('"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"','"+req.body.name+"')",(err,result) => {
+        if(err) console.log(err);
+        res.redirect('/career');
+    });
+}else{
 if(session.user_id !== undefined){
     config.con.query("SELECT * FROM user WHERE id="+session.user_id,(err,result) => {
         if(err){res.redirect('/logout');}else{
@@ -343,6 +356,7 @@ if(session.user_id !== undefined){
     });
 }else{
     res.render('career',{APP_URL : config.APP_URL,url:req.url,user:user});
+}
 }
 }
 exports.aboutus = function(req, res) {
@@ -647,7 +661,7 @@ if(req.body.booking_id !== undefined){
             res.redirect('/');
 }
 }
-exports.cancle = function(req, res) {
+exports.cancel = function(req, res) {
     session=req.session;
     let user = '';
 if(req.params.id){
